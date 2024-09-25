d7431a634d02f3512b44d332966c2d2ec9f240b9
M L <martinn.lindbladd@gmail.com>
2024-09-25 21:13:48 +0200

    builder bat no mkdir

--- builder.bat
+++ builder.bat
@@ -1,4 +1,1 @@
 venv\Scripts\python.exe -m PyInstaller main.py -n "Monitor" --onefile --collect-submodules "logger.py" --collect-submodules "menu.py" --collect-submodules "psmonitor.py" --collect-submodules "saver.py" --collect-submodules "textefficiency.py"
-mkdir dist\data
-mkdir dist\data\log
-mkdir dist\data\save
