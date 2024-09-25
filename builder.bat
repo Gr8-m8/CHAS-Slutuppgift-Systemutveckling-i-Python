4ac00df3701600ebc437a122ac9c174e22c88d0e
M L <martinn.lindbladd@gmail.com>
2024-09-25 20:43:55 +0200

    builder bat

---
+++ builder.bat
@@ -0,0 +1,1 @@
+venv\Scripts\python.exe -m PyInstaller main.py -n "Monitor" --onefile --collect-submodules "logger.py" --collect-submodules "menu.py" --collect-submodules "psmonitor.py" --collect-submodules "saver.py" --collect-submodules "textefficiency.py"
